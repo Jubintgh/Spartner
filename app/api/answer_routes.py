3744173b1841cd92cf51862a89185947d483382e
Joubin T <jubin.taghdir@gmail.com>
2021-07-17 13:23:53 -0700

    add answer apis

--- app/api/answer_routes.py
+++ app/api/answer_routes.py
@@ -1,1 +1,19 @@
-from flask 
+from flask.blueprints import Blueprint
+from flaskblueprints, 
+from app.models import Answer
+
+user_routes = Blueprint('answers', __name__)
+
+#error handling
+
+@user_routes.route('/:userid')
+def get_answers():
+    pass
+
+@user_routes.route('/:userid')
+def post_answers():
+    pass
+
+@user_routes.route('/:userid')
+def update_answer():
+    pass
