50331f555e032f713d98dd502484906642a2c7ef
Joubin T <jubin.taghdir@gmail.com>
2021-07-18 19:04:02 -0700

    add signup form

--- app/api/like_routes.py
+++ app/api/like_routes.py
@@ -1,22 +1,22 @@
 from flask import Blueprint, jsonify
 from app.models import Like
 
-user_routes = Blueprint('likes', __name__)
+like_routes = Blueprint('likes', __name__)
 
 #error handling
 
-@user_routes.route('/likes-you/:userid')
+@like_routes.route('/likes-you/:userid')
 def get_admirers():
     pass
 
-@user_routes.route('/you-like/:userid')
+@like_routes.route('/you-like/:userid')
 def get_likes():
     pass
 
-@user_routes.route('/', method=['POST'])
+@like_routes.route('/', method=['POST'])
 def make_like():
     pass
 
-@user_routes.route('/:userid', method=['DELETE'])
+@like_routes.route('/:userid', method=['DELETE'])
 def delete_like():
     pass
--- app/forms/signup_form.py
+++ app/forms/signup_form.py
@@ -1,5 +1,6 @@
+from os import abort
 from flask_wtf import FlaskForm
-from wtforms import StringField
+from wtforms import StringField, IntegerField, BooleanField
 from wtforms.validators import DataRequired, Email, ValidationError
 from app.models import User
 
@@ -21,6 +22,15 @@ def username_exists(form, field):
 
 
 class SignUpForm(FlaskForm):
+    first_name = StringField('first name', validators=[DataRequired()])
+    last_name = StringField('last name', validators=[DataRequired()])
+    age = IntegerField('age', validaros=[DataRequired()])
+    
+    location = StringField('location', validators=[DataRequired()])
+    gender = StringField('gender', validators=[DataRequired()])
+    coach = BooleanField('trainer')
+    fighter = BooleanField('looking for training partner')
+    image_url = StringField('Profile picture URL')
     username = StringField(
         'username', validators=[DataRequired(), username_exists])
     email = StringField('email', validators=[DataRequired(), user_exists])
