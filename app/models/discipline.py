ab8b90a23470b407aea543b1f9cb238cf8f38868
Matt Melendez <matthewemelendez@gmail.com>
2021-07-19 21:33:51 -0700

    discipline seeds done

--- app/models/discipline.py
+++ app/models/discipline.py
@@ -5,7 +5,5 @@ class Discipline(db.Model):
 
     id = db.Column(db.Integer, primary_key=True, nullable=False)
     discipline_name = db.Column(db.VARCHAR(20), nullable=False)
-    created_on = db.Column(db.DateTime, server_default=db.func.now())
-    updated_on = db.Column(db.DateTime, server_default=db.func.now(), server_onupdate=db.func.now())
 
     users_discipline = db.relationship('User_Discipline', back_populates='disciplines')
