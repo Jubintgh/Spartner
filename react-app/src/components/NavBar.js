fc39b60833a87ba46353a08498d072f207807d8b
AnthonyCost <anthonycost99@gmail.com>
2021-07-20 10:40:42 -0700

    positioning elements

--- react-app/src/components/NavBar.js
+++ react-app/src/components/NavBar.js
@@ -8,7 +8,7 @@ const NavBar = () => {
     <nav>
       <ul className="navbar">
         <li>
-          <NavLink to='/' exact={true} activeClassName='active'>
+          <NavLink id="navbar__brand-home" to='/' exact={true} activeClassName='active'>
             <img src='https://user-images.githubusercontent.com/35717793/126367109-4954f04b-0cb7-4ca9-a25a-d18e6b7cb74a.png' alt='logo' id='navbar__logo' />
           </NavLink>
         </li>
