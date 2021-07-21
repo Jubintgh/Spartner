5bb21785d6ed98fe9c33a6657e5c3a3ead98ff11
AnthonyCost <anthonycost99@gmail.com>
2021-07-20 20:49:15 -0700

    better formatted

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -6,6 +6,7 @@ import "./Footer.css";
 const Footer = () => {
     return (
         <div className="main-footer">
+<IconContext.Provider value={{ className: 'react-icons'}}>
             <div className="footer-container">
                 <div className="footer-title-container">
                 <img src="https://user-images.githubusercontent.com/35717793/126367109-4954f04b-0cb7-4ca9-a25a-d18e6b7cb74a.png" className="spartnerFooterLogo"/>
@@ -16,49 +17,41 @@ const Footer = () => {
                     <div className="footer-col-row1">
                         <img className="footer-profilePic" src="/images/Austin.jpg"/>
                         <h3>Austin Keumper</h3>
+                        <div className="test">
+                        <a href="https://github.com/AKuemper" target="_blank"><FaGithub/></a>
+                        <a href="https://www.linkedin.com/in/austin-kuemper-0b471b84/" target="_blank"><FaLinkedin/></a>
+                        </div>
                     </div>
                     {/* column2 */}
                     <div className="footer-col-row1">
                         <img className="footer-profilePic" src="/images/PlaceholderProfile.png"/>
                         <h3>Jubin Taghdir</h3>
+                        <div className="footer-row-row2">
+                        <a href="https://github.com/Jubintgh" target="_blank"><FaGithub/></a>
+                        <a href="https://www.linkedin.com/in/jubintaghdir/" target="_blank"><FaLinkedin/></a>
+                    </div>
                     </div>
                     {/* column3 */}
                     <div className="footer-col-row1">
                         <img className="footer-profilePic" src="/images/PlaceholderProfile.png"/>
                         <h3>Matthew Melendez</h3>
+                        <div className="footer-row-row2">
+                        <a href="https://github.com/melendezmatt" target="_blank"><FaGithub/></a>
+                        <a href="https://www.linkedin.com/in/matthew-melendez/" target="_blank"><FaLinkedin/></a>
+                    </div>
                     </div>
                     {/* column4 */}
                     <div className="footer-col-row1">
                         <img className="footer-profilePic" src="/images/Tony.jpg"/>
                         <h3>Tony Costanza</h3>
-                    </div>
-                </div>
-                <hr className="footer-seperator"/>
-                <IconContext.Provider value={{ className: 'react-icons'}}>
-                <div className="footer-content-row-2">
-                    {/* row1 */}
-                    <div className="footer-row-row2">
-                        <a href="https://github.com/AKuemper" target="_blank"><FaGithub/></a>
-                        <a href="https://www.linkedin.com/in/austin-kuemper-0b471b84/" target="_blank"><FaLinkedin/></a>
-                    </div>
-                    {/* row2 */}
-                    <div className="footer-row-row2">
-                        <a href="https://github.com/Jubintgh" target="_blank"><FaGithub/></a>
-                        <a href="https://www.linkedin.com/in/jubintaghdir/" target="_blank"><FaLinkedin/></a>
-                    </div>
-                    {/* row3 */}
-                    <div className="footer-row-row2">
-                        <a href="https://github.com/melendezmatt" target="_blank"><FaGithub/></a>
-                        <a href="https://www.linkedin.com/in/matthew-melendez/" target="_blank"><FaLinkedin/></a>
-                    </div>
-                    {/* row4 */}
-                    <div className="footer-row-row2">
+                        <div className="footer-row-row2">
                         <a href="https://github.com/AnthonyCost" target="_blank"><FaGithub/></a>
                         <a href="https://www.linkedin.com/in/anthony-costanza-64952820a/" target="_blank"><FaLinkedin/></a>
                     </div>
+                    </div>
+                </div>
                 </div>
                     </IconContext.Provider>
-            </div>
         </div>
         )
 }
