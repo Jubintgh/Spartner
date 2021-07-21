7f15174e2948abc41f45cdd46183e704def0a8fe
AnthonyCost <anthonycost99@gmail.com>
2021-07-20 19:54:01 -0700

    flexbox almost there also added images

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -5,26 +5,27 @@ const Footer = () => {
     return (
         <div className="main-footer">
             <div className="footer-container">
+                <h3 className="footer-title">Spartner was a collaboration by:</h3>
                 <div className="footer-content-row-1">
                     {/* column1 */}
                     <div className="footer-col-row1">
-                        <img/>
+                        <img className="footer-profilePic" src="/images/PlaceholderProfile.png"/>
                         <h3>Austin Keumper</h3>
                     </div>
                     {/* column2 */}
                     <div className="footer-col-row1">
-                        <img/>
-                        <h3>Name here</h3>
+                        <img className="footer-profilePic" src="/images/PlaceholderProfile.png"/>
+                        <h3>Jubin Taghdir</h3>
                     </div>
                     {/* column3 */}
                     <div className="footer-col-row1">
-                        <img/>
-                        <h3>Name here</h3>
+                        <img className="footer-profilePic" src="/images/PlaceholderProfile.png"/>
+                        <h3>Matthew Melendez</h3>
                     </div>
                     {/* column4 */}
                     <div className="footer-col-row1">
-                        <img/>
-                        <h3>Name here</h3>
+                        <img className="footer-profilePic" src="/images/Tony.jpg"/>
+                        <h3>Tony Costanza</h3>
                     </div>
                 </div>
                 <hr className="footer-seperator"/>
