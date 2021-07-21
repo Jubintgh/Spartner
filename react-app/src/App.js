b6f3e8786afd96d8c20605cdc01d765fd2795e87
AnthonyCost <anthonycost99@gmail.com>
2021-07-20 19:03:50 -0700

    wrapped app in container and excluded the footer

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -9,6 +9,7 @@ import UsersList from './components/UsersList';
 import User from './components/User';
 import { authenticate } from './store/session';
 import AnswersForm from './components/auth/AnswersForm';
+import Footer from './components/Footer';
 
 function App() {
   const [loaded, setLoaded] = useState(false);
@@ -26,29 +27,34 @@ function App() {
   }
 
   return (
-    <BrowserRouter>
-      <NavBar />
-      <Switch>
-        <Route path='/login' exact={true}>
-          <LoginForm />
-        </Route>
-        <Route path='/sign-up' exact={true}>
-          <SignUpForm />
-        </Route>
-        <ProtectedRoute path='/users' exact={true} >
-          <UsersList/>
-        </ProtectedRoute>
-        <ProtectedRoute path='/users/:userId' exact={true} >
-          <User />
-        </ProtectedRoute>
-        <ProtectedRoute path='/' exact={true} >
-          <h1>My Home Page</h1>
-        </ProtectedRoute>
-        <Route path='/users/:userId/answers' exact={true}>
-          <AnswersForm />
-        </Route>
-      </Switch>
-    </BrowserRouter>
+    <div className="page-container">
+      <div className="content-wrap">
+        <BrowserRouter>
+          <NavBar />
+            <Switch>
+              <Route path='/login' exact={true}>
+                <LoginForm />
+              </Route>
+              <Route path='/sign-up' exact={true}>
+                <SignUpForm />
+              </Route>
+              <ProtectedRoute path='/users' exact={true} >
+                <UsersList/>
+              </ProtectedRoute>
+              <ProtectedRoute path='/users/:userId' exact={true} >
+                <User />
+              </ProtectedRoute>
+              <ProtectedRoute path='/' exact={true} >
+                <h1>My Home Page</h1>
+              </ProtectedRoute>
+              <Route path='/users/:userId/answers' exact={true}>
+                <AnswersForm />
+              </Route>
+            </Switch>
+        </BrowserRouter>
+        </div>
+        <Footer />
+    </div>
   );
 }
 
