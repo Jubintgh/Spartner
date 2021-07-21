e6affd44644d3f4ab90544704145e4c61738b0d6
AnthonyCost <anthonycost99@gmail.com>
2021-07-20 19:12:42 -0700

    adding basic skeleton in Footer component

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -1,9 +1,56 @@
 import React from 'react';
+import "./Footer.css";
 
 const Footer = () => {
     return (
-        <div className="footer">
-            <h2>Testing</h2>
+        <div className="main-footer">
+            <div className="footer-container">
+                <div className="footer-content-row-1">
+                    {/* column1 */}
+                    <div className="footer-col-row1">
+                        <img/>
+                        <h3>Name here</h3>
+                    </div>
+                    {/* column2 */}
+                    <div className="footer-col-row1">
+                        <img/>
+                        <h3>Name here</h3>
+                    </div>
+                    {/* column3 */}
+                    <div className="footer-col-row1">
+                        <img/>
+                        <h3>Name here</h3>
+                    </div>
+                    {/* column4 */}
+                    <div className="footer-col-row1">
+                        <img/>
+                        <h3>Name here</h3>
+                    </div>
+                </div>
+                <hr className="footer-seperator"/>
+                <div className="footer-content-row-2">
+                    {/* row1 */}
+                    <div className="footer-row-row2">
+                        <a><img/></a>
+                        <a><img/></a>
+                    </div>
+                    {/* row2 */}
+                    <div className="footer-row-row2">
+                        <a><img/></a>
+                        <a><img/></a>
+                    </div>
+                    {/* row3 */}
+                    <div className="footer-row-row2">
+                        <a><img/></a>
+                        <a><img/></a>
+                    </div>
+                    {/* row4 */}
+                    <div className="footer-row-row2">
+                        <a><img/></a>
+                        <a><img/></a>
+                    </div>
+                </div>
+            </div>
         </div>
         )
 }
