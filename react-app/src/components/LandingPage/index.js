62bdbdbe7308e67769881a8062316cac3fa77bdf
AnthonyCost <anthonycost99@gmail.com>
2021-07-22 11:16:29 -0700

    buttons styled and flexbox set up

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./LandingPage.css"
 
 const LandingPage = () => {
 
   return (
-    <div>
-      <h1>Welcome to Spartner</h1>
-      <btn>Log In</btn>
-      <btn>Sign Up</btn>
+      <div className="splash-background">
+    <div className="splash-container">
+        <div className="splash-banner">
+      <h1>Spartner</h1>
+      <h3>Have the match of a lifetime</h3>
+        </div>
+      <div className="splash-buttons">
+      <button>Log In</button>
+      <button>Sign Up</button>
+      </div>
     </div>
+      </div>
   );
   }
 export default LandingPage;
