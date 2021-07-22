4dab83250e3371dc78e0ec22c4bdc2b4cab4d455
AnthonyCost <anthonycost99@gmail.com>
2021-07-22 13:15:01 -0700

    added fade in animation on load

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -8,7 +8,7 @@ const LandingPage = () => {
       <div className="splash-background">
     <div className="splash-container">
         <div className="splash-banner">
-      <img src="https://user-images.githubusercontent.com/35717793/126691381-d2839a71-94f6-4343-836d-216d8b97e9ec.png" className="splash-header" />
+      <img src="https://user-images.githubusercontent.com/35717793/126702199-828042e7-7543-47e1-b3d3-67539211241e.png" className="splash-header" />
       <h3 className="splash-subheader">Have the match of a lifetime</h3>
         </div>
       <div className="splash-buttons">
