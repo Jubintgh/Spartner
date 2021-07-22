b1d32c34fb29bfd0a7c592b26a41910f66d09dfe
AnthonyCost <anthonycost99@gmail.com>
2021-07-22 10:53:54 -0700

    initialized landing page

---
+++ react-app/src/components/LandingPage/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const LandingPage = () => {
+
+  return (
+    <div>
+      <h1>Welcome to Spartner</h1>
+      <btn>Log In</btn>
+      <btn>Sign Up</btn>
+    </div>
+  );
+  }
+export default LandingPage;
