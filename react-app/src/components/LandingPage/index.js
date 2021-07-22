01df87ae216636499dc40b1a46855a416fdb3ebc
AnthonyCost <anthonycost99@gmail.com>
2021-07-22 11:41:37 -0700

    almost fully styled, just need to tweak the subheader and positioning of the divs

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -8,8 +8,8 @@ const LandingPage = () => {
       <div className="splash-background">
     <div className="splash-container">
         <div className="splash-banner">
-      <h1>Spartner</h1>
-      <h3>Have the match of a lifetime</h3>
+      <img src="https://user-images.githubusercontent.com/35717793/126691381-d2839a71-94f6-4343-836d-216d8b97e9ec.png" className="splash-header" />
+      <h3 className="splash-subheader">Have the match of a lifetime</h3>
         </div>
       <div className="splash-buttons">
       <button>Log In</button>
