b95575e3fcf50c10aeed2278c4c5e7d431abe80f
Matt Melendez <matthewemelendez@gmail.com>
2021-07-22 20:48:20 -0700

    answers seed almost done

---
+++ app/seeds/answersCopy.py
@@ -0,0 +1,311 @@
+from app.models import db, Answer
+
+
+    answer31= Answer(
+        user_id=31, about="They call me Hummus", weight_class=1, reach=55,
+        professional_level=0, fav_rocky_fighter="Apollo Creed", vaccinated=True,
+        nickname="Joey B", walkout_song="'Boyfriend' by Justin Bieber", availability=1,
+        has_kids=False, religion='Atheist', pets='Justin', rate=200, in_person=True)
+
+    answer32= Answer(
+        user_id=32, about="They call me Hummus", weight_class=2, reach=56,
+        professional_level=1, fav_rocky_fighter="Ivan Drago", vaccinated=False,
+        nickname="Drake", walkout_song="'Beauty And A Beat' by Justin Bieber", availability=2,
+        has_kids=True, religion='Hindu', pets='Bieber', rate=100, in_person=False)
+
+    answer33= Answer(
+        user_id=33, about="They call me Hummus", weight_class=3, reach=57,
+        professional_level=2, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Yeezy", walkout_song="'Sweatpants' by Childish Gambino", availability=1,
+        has_kids=False, religion='Hindu', pets='Iron Man', rate=20, in_person=True)
+
+    answer34= Answer(
+        user_id=34, about="They call me Hummus", weight_class=4, reach=58,
+        professional_level=0, fav_rocky_fighter="Ivan Drago", vaccinated=False,
+        nickname="Childish", walkout_song="'3005' by Childish Gambino", availability=2,
+        has_kids=True, religion='Atheist', pets='Vitamin B12', rate=10, in_person=False)
+
+    answer35= Answer(
+        user_id=35, about="They call me Hummus", weight_class=5, reach=59,
+        professional_level=1, fav_rocky_fighter="Apollo Creed", vaccinated=True,
+        nickname="Chance", walkout_song="'Chanel' by Frank Ocean", availability=1,
+        has_kids=True, religion='Roman Catholic', pets='Childish', rate=1, in_person=True)
+
+    answer36= Answer(
+        user_id=36, about="They call me Hummus", weight_class=6, reach=60,
+        professional_level=1, fav_rocky_fighter="Ivan Drago", vaccinated=True,
+        nickname="Hermione", walkout_song="'No Problem' by Chance The Rapper", availability=2,
+        has_kids=False, religion='Muslim', pets='Chance', rate=25, in_person=False)
+
+    answer37= Answer(
+        user_id=37, about="They call me Hummus", weight_class=0, reach=61,
+        professional_level=2, fav_rocky_fighter="Rocky Balboa", vaccinated=False,
+        nickname="Dorito", walkout_song="'Last Time That I Checc'd' by Nipsey Hussle", availability=0,
+        has_kids=True, religion='Atheist', pets='Nip', rate=50, in_person=True)
+
+    answer38= Answer(
+        user_id=38, about="They call me Hummus", weight_class=1, reach=62,
+        professional_level=2, fav_rocky_fighter="Ivan Drago", vaccinated=False,
+        nickname="Champ", walkout_song="'FDT' by YG", availability=0,
+        has_kids=False, religion='Muslim', pets='T-Bone', rate=50, in_person=False)
+    answer39= Answer(
+        user_id=39, about="They call me Hummus", weight_class=2, reach=63,
+        professional_level=1, fav_rocky_fighter="Apollo Creed", vaccinated=True,
+        nickname="Coke Zero", walkout_song="'I Just Wanna Party' by YG", availability=2,
+        has_kids=True, religion='Hindu', pets='Woman', rate=50, in_person=True)
+
+
+
+    answer40= Answer(
+        user_id=40, about="They call me Hummus", weight_class=3, reach=64,
+        professional_level=1, fav_rocky_fighter="Ivan Drago", vaccinated=True,
+        nickname="Cricket", walkout_song="'Toot It and Boot It' by YG", availability=1,
+        has_kids=False, religion='Hindu', pets='Man', rate=1000, in_person=False)
+
+    answer41= Answer(
+        user_id=41, about="They call me Hummus", weight_class=4, reach=65,
+        professional_level=2, fav_rocky_fighter="Adonis Johnson", vaccinated=True,
+        nickname="Twix", walkout_song="'Stay With Me' by Sam Smith", availability=0,
+        has_kids=True, religion='Roman Catholic', pets='YG', rate=5000, in_person=True)
+
+
+
+    answer42= Answer(
+        user_id=42, about="They call me Hummus", weight_class=5, reach=66,
+        professional_level=1, fav_rocky_fighter="Ivan Drago", vaccinated=True,
+        nickname="Cherry", walkout_song="'WAP' by Cardi B", availability=2,
+        has_kids=True, religion='Hindu', pets='Sparky The Dog', rate=5050, in_person=False)
+
+    answer43= Answer(
+        user_id=43, about="They call me Hummus", weight_class=6, reach=66,
+        professional_level=1, fav_rocky_fighter="Apollo Creed", vaccinated=True,
+        nickname="Cloud", walkout_song="'Redbone' by Childish Gambino", availability=2,
+        has_kids=False, religion='Atheist', pets='10 Sharks', rate=50, in_person=True)
+
+    answer44= Answer(
+        user_id=44, about="They call me Hummus", weight_class=0, reach=67,
+        professional_level=2, fav_rocky_fighter="Rocky Balboa", vaccinated=False,
+        nickname="Knucklebutt", walkout_song="'Scorpion' by Drake", availability=2,
+        has_kids=True, religion='Muslim', pets='DogCat', rate=10, in_person=False)
+
+    answer45= Answer(
+        user_id=45, about="They call me Hummus", weight_class=0, reach=67,
+        professional_level=1, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Pinata", walkout_song="'Marvin's Room' by Drake", availability=2,
+        has_kids=False, religion='Muslim', pets='CatDog', rate=10, in_person=True)
+
+    answer46= Answer(
+        user_id=46, about="They call me Hummus", weight_class=1, reach=67,
+        professional_level=1, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="The King", walkout_song="'What's My Age Again?' by Blink-182", availability=1,
+        has_kids=True, religion='Atheist', pets='Kyedae', rate=10, in_person=False)
+
+    answer47= Answer(
+        user_id=47, about="They call me Hummus", weight_class=1, reach=68,
+        professional_level=0, fav_rocky_fighter="Ivan Drago", vaccinated=False,
+        nickname="Big Nasty", walkout_song="'All The Small Things' by Blink-182", availability=2,
+        has_kids=False, religion='Buddhist', pets='Spar', rate=500, in_person=False)
+
+    answer48= Answer(
+        user_id=48, about="They call me Hummus", weight_class=2, reach=68,
+        professional_level=1, fav_rocky_fighter="Apollo Creed", vaccinated=True,
+        nickname="Joker", walkout_song="'Love Me Harder' by Ariana Grande", availability=1,
+        has_kids=True, religion='Hindu', pets='5 Cats', rate=500, in_person=True)
+
+    answer49= Answer(
+        user_id=49, about="They call me Hummus", weight_class=2, reach=69,
+        professional_level=1, fav_rocky_fighter="Rocky Balboa", vaccinated=False,
+        nickname="Snickerdoodle", walkout_song="'Starships' by Nicki Minaj", availability=2,
+        has_kids=True, religion='Roman Catholic', pets='Golfwang', rate=500, in_person=False)
+
+    answer50= Answer(
+        user_id=50, about="They call me Hummus", weight_class=3, reach=69,
+        professional_level=2, fav_rocky_fighter="Ivan Drago", vaccinated=True,
+        nickname="Gingersnap", walkout_song="'Monsters' by Kanye West", availability=2,
+        has_kids=False, religion='Muslim', pets='Taco', rate=500, in_person=True)
+
+    answer51= Answer(
+        user_id=51, about="They call me Hummus", weight_class=3, reach=70,
+        professional_level=2, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Rainbow", walkout_song="'Blood on the Leaves' by Kanye West", availability=1,
+        has_kids=True, religion='Roman Catholic', pets='Gata', rate=500, in_person=False)
+
+    answer52= Answer(
+        user_id=52, about="They call me Hummus", weight_class=4, reach=70,
+        professional_level=2, fav_rocky_fighter="Ivan Drago", vaccinated=True,
+        nickname="Mouse", walkout_song="'Professional Rapper' by Lil Dicky", availability=2,
+        has_kids=False, religion='Christian', pets='Dave Burd', rate=300, in_person=False)
+
+    answer53= Answer(
+        user_id=53, about="They call me Hummus", weight_class=4, reach=71,
+        professional_level=1, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Pickle", walkout_song="'WAWA' by Party Favor", availability=1,
+        has_kids=True, religion='Roman Catholic', pets='Sparkz the Second', rate=300, in_person=True)
+
+    answer54= Answer(
+        user_id=54, about="They call me Hummus", weight_class=4, reach=72,
+        professional_level=1, fav_rocky_fighter="Rocky Balboa", vaccinated=False,
+        nickname="Con", walkout_song="'Runaway' by Galantis", availability=2,
+        has_kids=False, religion='Hindu', pets='Klawhi', rate=20, in_person=False)
+
+    answer55= Answer(
+        user_id=55, about="They call me Hummus", weight_class=5, reach=73,
+        professional_level=1, fav_rocky_fighter="Adonis Johnson", vaccinated=False,
+        nickname="Green Giant", walkout_song="'NASA' by Ariana Grande", availability=1,
+        has_kids=False, religion='Christian', pets='The Brow', rate=20, in_person=True)
+
+    answer56= Answer(
+        user_id=56, about="They call me Hummus", weight_class=5, reach=74,
+        professional_level=0, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Mad Max", walkout_song="'34+35' by Ariana Grande", availability=2,
+        has_kids=True, religion='Hindu', pets='King James', rate=500, in_person=False)
+
+    answer57= Answer(
+        user_id=57, about="They call me Hummus", weight_class=5, reach=75,
+        professional_level=0, fav_rocky_fighter="Ivan Drago", vaccinated=False,
+        nickname="Amethyst", walkout_song="'Break Free' by Ariana Grande", availability=2,
+        has_kids=True, religion='Roman Catholic', pets='Jisoo', rate=500, in_person=True)
+
+    answer58= Answer(
+        user_id=58, about="They call me Hummus", weight_class=6, reach=76,
+        professional_level=1, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Bub", walkout_song="'That Should Be Me' by Justin Bieber", availability=0,
+        has_kids=True, religion='Jewish', pets='Rose', rate=20, in_person=False)
+
+
+
+    answer59= Answer(
+        user_id=59, about="They call me Hummus", weight_class=6, reach=77,
+        professional_level=1, fav_rocky_fighter="Rocky Balboa", vaccinated=False,
+        nickname="Babs", walkout_song="'Homecoming' by Kanye West", availability=2,
+        has_kids=False, religion='Christian', pets='Jennie', rate=50, in_person=False)
+
+    answer60= Answer(
+        user_id=60, about="They call me Hummus", weight_class=6, reach=76,
+        professional_level=0, fav_rocky_fighter="Rocky Balboa", vaccinated=False,
+        nickname="Twinkie", walkout_song="'All I Want For Christmas' by Mariah Carey", availability=0,
+        has_kids=True, religion='Jewish', pets='Lisa', rate=10, in_person=True)
+
+    answer61= Answer(
+        user_id=61, about="They call me Hummus", weight_class=6, reach=76,
+        professional_level=2, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Brown Sugar", walkout_song="'All I Want For Christmas' by Mariah Carey", availability=2,
+        has_kids=False, religion='Roman Catholic', pets='Doja Cat', rate=500, in_person=True)
+
+
+
+    answer62= Answer(
+        user_id=62, about="They call me Hummus", weight_class=1, reach=75,
+        professional_level=0, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Robin", walkout_song="'All I Want For Christmas' by Mariah Carey", availability=1,
+        has_kids=True, religion='Christian', pets='Sparkz', rate=30, in_person=True)
+
+    answer63= Answer(
+        user_id=63, about="They call me Hummus", weight_class=1, reach=74,
+        professional_level=0, fav_rocky_fighter="Rocky Balboa", vaccinated=False,
+        nickname="Papito", walkout_song="'All I Want For Christmas' by Mariah Carey", availability=2,
+        has_kids=False, religion='Jewish', pets='Dog and Cat', rate=500, in_person=False)
+
+    answer64= Answer(
+        user_id=64, about="They call me Hummus", weight_class=2, reach=74,
+        professional_level=1, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Goldilocks", walkout_song="'Bonfire' by Childish Gambino", availability=2,
+        has_kids=True, religion='Buddhist', pets='Big Bear', rate=500, in_person=True)
+
+    answer65= Answer(
+        user_id=65, about="They call me Hummus", weight_class=2, reach=73,
+        professional_level=1, fav_rocky_fighter="Ivan Drago", vaccinated=False,
+        nickname="Bebe", walkout_song="'Bounce Back' by Big Sean", availability=0,
+        has_kids=True, religion='Jewish', pets='Spinal the Snake', rate=500, in_person=False)
+
+    answer66= Answer(
+        user_id=66, about="They call me Hummus", weight_class=3, reach=72,
+        professional_level=2, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="MomBod", walkout_song="'Redbone' by Childish Gambino", availability=0,
+        has_kids=False, religion='Roman Catholic',  rate=40, in_person=False)
+
+    answer67= Answer(
+        user_id=67, about="They call me Hummus", weight_class=3, reach=70,
+        professional_level=0, fav_rocky_fighter="Adonis Johnson", vaccinated=False,
+        nickname="Pretty Lady", walkout_song="'Bound 2' by Kanye West", availability=2,
+        has_kids=True, religion='Roman Catholic', pets='Yeezus Christ', rate=500, in_person=False)
+
+    answer68= Answer(
+        user_id=68, about="They call me Hummus", weight_class=3, reach=71,
+        professional_level=1, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Butternut", walkout_song="'Cocoa Butter Kisses' by Chance The Rapper", availability=1,
+        has_kids=True, religion='Jewish', rate=500, in_person=True)
+
+    answer69= Answer(
+        user_id=69, about="They call me Hummus", weight_class=0, reach=69,
+        professional_level=0, fav_rocky_fighter="Adonis Johnson", vaccinated=True,
+        nickname="Lobster", walkout_song="'Redbone' by Childish Gambino", availability=1,
+        has_kids=True, religion='Roman Catholic', rate=50, in_person=True)
+
+    answer70= Answer(
+        user_id=70, about="They call me Hummus", weight_class=0, reach=68,
+        professional_level=0, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Schnookums", walkout_song="'Molly' by Lil Dicky", availability=1,
+        has_kids=True, religion='Buddhist', pets='Lovebug', rate=50000, in_person=False)
+
+    answer71= Answer(
+        user_id=71, about="They call me Hummus", weight_class=0, reach=67,
+        professional_level=2, fav_rocky_fighter="Adonis Johnson", vaccinated=False,
+        nickname="Buds", walkout_song="'Panda' by Desiigner", availability=2,
+        has_kids=False, religion='Jewish', pets='Cheesy', rate=30, in_person=True)
+
+
+
+    answer72= Answer(
+        user_id=72, about="They call me Hummus", weight_class=4, reach=66,
+        professional_level=2, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Diesel", walkout_song="'Congratulations' by Post Malone", availability=2,
+        has_kids=True, religion='Roman Catholic', rate=50000, in_person=False)
+
+    answer73= Answer(
+        user_id=73, about="They call me Hummus", weight_class=3, reach=65,
+        professional_level=0, fav_rocky_fighter="Adonis Johnson", vaccinated=True,
+        nickname="Chewbacca", walkout_song="'Freaks and Geeks' by Childish Gambino", availability=2,
+        has_kids=False, religion='Roman Catholic', pets='Sparky The Dog', rate=50000, in_person=True)
+
+    answer74= Answer(
+        user_id=74, about="They call me Hummus", weight_class=3, reach=64,
+        professional_level=1, fav_rocky_fighter="Rocky Balboa", vaccinated=False,
+        nickname="Cheeky", walkout_song="'Feels Like Summer' by Childish Gambino", availability=1,
+        has_kids=True, religion='Roman Catholic', pets='Sparky The Dog', rate=500, in_person=True)
+
+    answer75= Answer(
+        user_id=75, about="They call me Hummus", weight_class=5, reach=64,
+        professional_level=1, fav_rocky_fighter="Adonis Johnson", vaccinated=True,
+        nickname="Gordo", walkout_song="'My Own Thing' by Chance The Rapper", availability=0,
+        has_kids=False, religion='Jewish',rate=500, in_person=False)
+
+    answer76= Answer(
+        user_id=76, about="They call me Hummus", weight_class=5, reach=65,
+        professional_level=2, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Ghoulie", walkout_song="'Pretty Savage' by BLACKPINK", availability=0,
+        has_kids=True, religion='Roman Catholic', rate=10, in_person=True)
+
+    answer77= Answer(
+        user_id=77, about="They call me Hummus", weight_class=6, reach=66,
+        professional_level=2, fav_rocky_fighter="Adonis Johnson", vaccinated=False,
+        nickname="Ace", walkout_song="'Lovesick Girls' by BLACKPINK", availability=0,
+        has_kids=False, religion='Roman Catholic', pets='Sparky The Dog', rate=25, in_person=False)
+
+    answer78= Answer(
+        user_id=78, about="They call me Hummus", weight_class=6, reach=67,
+        professional_level=2, fav_rocky_fighter="Rocky Balboa", vaccinated=True,
+        nickname="Toots", walkout_song="'Hometown Girl' by ZHU", availability=1,
+        has_kids=True, religion='Buddhist', pets='Ssssebastian the Snake', rate=25, in_person=True)
+
+    answer79= Answer(
+        user_id=79, about="They call me Hummus", weight_class=6, reach=68,
+        professional_level=1, fav_rocky_fighter="Adonis Johnson", vaccinated=False,
+        nickname="Grease", walkout_song="'Living Single' by Big Sean", availability=2,
+        has_kids=True, religion='Jewish', pets='3 Cats', rate=50, in_person=True)
+
+    answer80= Answer(
+        user_id=80, about="They call me Hummus", weight_class=1, reach=69,
+        professional_level=0, fav_rocky_fighter="Adonis Johnson", vaccinated=True,
+        nickname="Wilma", walkout_song="'Single Again' by Big Sean", availability=1,
+        has_kids=False, religion='Jewish', pets='3 Dogs', rate=500, in_person=False)
