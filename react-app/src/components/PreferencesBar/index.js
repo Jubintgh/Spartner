cca569e7fa5712e2fb9d40821569deaf43a46f3c
AnthonyCost <anthonycost99@gmail.com>
2021-07-23 15:57:01 -0700

    added missing divs

--- react-app/src/components/PreferencesBar/index.js
+++ react-app/src/components/PreferencesBar/index.js
@@ -26,59 +26,7 @@ const changeImageSourceLiked = (e) => {
 
 */ 
 
-let renderCoachContent = null;
-
-  if (!isCoach) {
-    renderCoachContent = (
-        <div className="element">
-        <div className="icon">
-            <img/>
-        </div>
-        <div className="icon-label">
-            <h5>Coaches</h5>
-        </div>)
-};
-  
-  return (
-    <div className="block">
-      <div className="element">
-        <div className="icon">
-            <img/>
-        </div>
-        <div className="icon-label">
-            <h5>Recommended</h5>
-        </div>
-      </div>
-      <div className="element">
-        <div className="icon">
-            <img/>
-        </div>
-        <div className="icon-label">
-            <h5>Weight Class</h5>
-        </div>
-      </div>
-      <div className="element">
-        <div className="icon">
-            <img/>
-        </div>
-        <div className="icon-label">
-            <h5>Vaccinated</h5>
-        </div>
-      </div>
-      <div className="element">
-        <div className="icon">
-            <img/>
-        </div>
-        <div className="icon-label">
-            <h5>Professional Level</h5>
-        </div>
-      </div>
-      {renderCoachContent}
-      </div>
-    </div>
-  );
-};
-{/* { user? <div className="nav-logo">
+/* { user? <div className="nav-logo">
     <Link id="navbar__brand-home" to='/discover' exact={true} activeClassName='active'>
         <img src='https://user-images.githubusercontent.com/35717793/126367109-4954f04b-0cb7-4ca9-a25a-d18e6b7cb74a.png' alt='logo' id='navbar__logo' />
     </Link>
@@ -87,6 +35,60 @@ let renderCoachContent = null;
         <img src='https://user-images.githubusercontent.com/35717793/126367109-4954f04b-0cb7-4ca9-a25a-d18e6b7cb74a.png' alt='logo' id='navbar__logo' />
     </Link>
   </div>}
-{navContent} */};
+{navContent} */;
+
+let renderCoachContent = null;
+
+  if (!isCoach) {
+    renderCoachContent = (
+        <div className="element">
+            <div className="icon">
+                <img/>
+            </div>
+            <div className="icon-label">
+                <h5>Coaches</h5>
+            </div>
+        </div>
+        )
+  }
+  
+return (
+        <div className="block">
+            <div className="element">
+                <div className="icon">
+                    <img/>
+                </div>
+                <div className="icon-label">
+                    <h5>Recommended</h5>
+                </div>
+            </div>
+            <div className="element">
+                <div className="icon">
+                    <img/>
+                </div>
+                <div className="icon-label">
+                    <h5>Weight Class</h5>
+                </div>
+            </div>
+            <div className="element">
+                <div className="icon">
+                    <img/>
+                </div>
+                <div className="icon-label">
+                    <h5>Vaccinated</h5>
+                </div>
+            </div>
+            <div className="element">
+                <div className="icon">
+                    <img/>
+                </div>
+                <div className="icon-label">
+                    <h5>Professional Level</h5>
+                </div>
+            </div>
+        {renderCoachContent}
+    </div>
+  );
+};
 
 export default PreferencesBar
