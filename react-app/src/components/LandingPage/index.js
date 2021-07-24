08df83a9435b10b5ef1f0f424dee30356adc1a5c
Austin Kuemper <78562233+AKuemper@users.noreply.github.com>
2021-07-24 11:38:33 -0700

    redirect to discover after demo user button is selected, added demo user button to landing page, redirect to landing page when user presses the logout button

--- react-app/src/components/LandingPage/index.js
+++ react-app/src/components/LandingPage/index.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import "./LandingPage.css"
+import './LandingPage.css';
+import DemoUserButton from '../auth/DemoUserButton';
 
 const LandingPage = () => {
-
   return (
-      <div className="splash-background">
-    <div className="splash-container">
-        <div className="splash-banner">
-      <img src="https://user-images.githubusercontent.com/35717793/126702199-828042e7-7543-47e1-b3d3-67539211241e.png" className="splash-header" />
-      <h3 className="splash-subheader">Have the match of a lifetime</h3>
+    <div className='splash-background'>
+      <div className='splash-container'>
+        <div className='splash-banner'>
+          <img
+            src='https://user-images.githubusercontent.com/35717793/126702199-828042e7-7543-47e1-b3d3-67539211241e.png'
+            className='splash-header'
+          />
+          <h3 className='splash-subheader'>Have the match of a lifetime</h3>
         </div>
-      <div className="splash-buttons">
+        <div className='splash-buttons'>
           <Link to='/login'>
-      <button>Log In</button>
+            <button>Log In</button>
           </Link>
           <Link to='/sign-up'>
-      <button>Sign Up</button>
+            <button>Sign Up</button>
           </Link>
+          <DemoUserButton />
+        </div>
       </div>
     </div>
-      </div>
   );
-  }
+};
 export default LandingPage;
