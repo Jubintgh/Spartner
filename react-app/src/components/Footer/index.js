ae4347d5a9d99198c41d5842b94704831df8d867
Austin Kuemper <78562233+AKuemper@users.noreply.github.com>
2021-08-25 15:42:57 -0700

    matt and austin links updated

--- react-app/src/components/Footer/index.js
+++ react-app/src/components/Footer/index.js
@@ -12,7 +12,7 @@ const Footer = () => {
         <div className='footer-container'>
           <div className='footer-title-container'>
             <img
-              alt="logo"
+              alt='logo'
               src='https://user-images.githubusercontent.com/35717793/126367109-4954f04b-0cb7-4ca9-a25a-d18e6b7cb74a.png'
               className='spartnerFooterLogo'
             />
@@ -21,21 +21,37 @@ const Footer = () => {
           <div className='footer-content-row-1'>
             {/* Austin */}
             <div className='footer-col-row1'>
-              <img alt="AKphoto" className='footer-profilePic' src='https://avatars.githubusercontent.com/u/78562233?v=4'/>
+              <img
+                alt='AKphoto'
+                className='footer-profilePic'
+                src='https://avatars.githubusercontent.com/u/78562233?v=4'
+              />
               <h3>Austin Kuemper</h3>
               <div className='footer-row-row2'>
-                <a rel="noreferrer" href='https://github.com/AKuemper' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://github.com/AKuemper'
+                  target='_blank'
+                >
                   <FaGithub />
                 </a>
-                <a rel="noreferrer" href='https://angel.co/u/austin-kuemper' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://angel.co/u/austin-kuemper'
+                  target='_blank'
+                >
                   <FaAngellist />
                 </a>
-                <a rel="noreferrer" href='https://github.com/AKuemper' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://www.austinkuemper.dev/'
+                  target='_blank'
+                >
                   <MdWeb />
                 </a>
                 <a
-                  rel="noreferrer"
-                  href='https://www.linkedin.com/in/austin-kuemper-0b471b84/'
+                  rel='noreferrer'
+                  href='https://www.linkedin.com/in/austinkuemper/'
                   target='_blank'
                 >
                   <FaLinkedin />
@@ -44,20 +60,36 @@ const Footer = () => {
             </div>
             {/* Jubin */}
             <div className='footer-col-row1'>
-              <img alt="jubinPic" className='footer-profilePic' src='https://avatars.githubusercontent.com/u/73211975?v=4' />
+              <img
+                alt='jubinPic'
+                className='footer-profilePic'
+                src='https://avatars.githubusercontent.com/u/73211975?v=4'
+              />
               <h3>Jubin Taghdir</h3>
               <div className='footer-row-row2'>
-                <a rel="noreferrer" href='https://github.com/Jubintgh' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://github.com/Jubintgh'
+                  target='_blank'
+                >
                   <FaGithub />
                 </a>
-                <a rel="noreferrer" href='https://angel.co/u/jubin-taghdir' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://angel.co/u/jubin-taghdir'
+                  target='_blank'
+                >
                   <FaAngellist />
                 </a>
-                <a rel="noreferrer" href='https://github.com/Jubintghr' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://github.com/Jubintghr'
+                  target='_blank'
+                >
                   <MdWeb />
                 </a>
                 <a
-                  rel="noreferrer"
+                  rel='noreferrer'
                   href='https://www.linkedin.com/in/jubintaghdir/'
                   target='_blank'
                 >
@@ -67,20 +99,36 @@ const Footer = () => {
             </div>
             {/* Matt */}
             <div className='footer-col-row1'>
-              <img alt="MattPhoto" className='footer-profilePic' src='https://avatars.githubusercontent.com/u/79602970?v=4' />
+              <img
+                alt='MattPhoto'
+                className='footer-profilePic'
+                src='https://avatars.githubusercontent.com/u/79602970?v=4'
+              />
               <h3>Matthew Melendez</h3>
               <div className='footer-row-row2'>
-                <a rel="noreferrer" href='https://github.com/melendezmatt' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://github.com/melendezmatt'
+                  target='_blank'
+                >
                   <FaGithub />
                 </a>
-                <a rel="noreferrer" href='https://angel.co/u/matthew-melendez-1' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://angel.co/u/matthew-melendez-1'
+                  target='_blank'
+                >
                   <FaAngellist />
                 </a>
-                <a rel="noreferrer" href='https://github.com/melendezmatt' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://melendezmatt.github.io'
+                  target='_blank'
+                >
                   <MdWeb />
                 </a>
                 <a
-                  rel="noreferrer"
+                  rel='noreferrer'
                   href='https://www.linkedin.com/in/matthew-melendez/'
                   target='_blank'
                 >
@@ -90,24 +138,36 @@ const Footer = () => {
             </div>
             {/* Tony */}
             <div className='footer-col-row1'>
-              <img alt="TonyPic" className='footer-profilePic' src='https://avatars.githubusercontent.com/u/35717793?v=4' />
+              <img
+                alt='TonyPic'
+                className='footer-profilePic'
+                src='https://avatars.githubusercontent.com/u/35717793?v=4'
+              />
               <h3>Tony Costanza</h3>
               <div className='footer-row-row2'>
-                <a rel="noreferrer" href='https://github.com/AnthonyCost' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://github.com/AnthonyCost'
+                  target='_blank'
+                >
                   <FaGithub />
                 </a>
-                <a rel="noreferrer" href='https://angel.co/u/anthony-costanza-1' target='_blank'>
+                <a
+                  rel='noreferrer'
+                  href='https://angel.co/u/anthony-costanza-1'
+                  target='_blank'
+                >
                   <FaAngellist />
                 </a>
                 <a
-                  rel="noreferrer"
+                  rel='noreferrer'
                   href='https://anthonycost.github.io/portfolio/'
                   target='_blank'
                 >
                   <MdWeb />
                 </a>
                 <a
-                  rel="noreferrer"
+                  rel='noreferrer'
                   href='https://www.linkedin.com/in/anthony-costanza-64952820a/'
                   target='_blank'
                 >
